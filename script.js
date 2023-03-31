f533469c28fa9f8f085814d192a05f4888abad79
Tarek_1230 <109407872+Fogsmen@users.noreply.github.com>
2023-03-31 06:16:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -50,3 +50,68 @@ document.querySelector('.home').onmouseleave = () =>{
 
     document.querySelector('.home .home-parallax-img').style.transform = `translateX(0px) translateY(0px)`;
 }   
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+	// set options
+	var speed = 500;           //fade speed
+	var autoSwitch = true;      //auto slider option
+	var autoSwitchSpeed = 4000; //auto slider speed
+
+	// add initial active class
+	$('.slide').first().addClass('active');
+
+	// hide all slides
+	$('.slide').hide();
+
+	// show first slide
+	$('.active').show();
+
+
+	// next button to go forward
+	$('#next').on('click', nextSlide);
+
+
+	// previous button to go back
+	$('#prev').on('click', prevSlide);
+
+	// auto slider
+	if(autoSwitch == true){
+		setInterval(nextSlide,autoSwitchSpeed);
+	}
+
+// switches to next slide
+function nextSlide(){
+	$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':last-child')){
+			$('.slide').first().addClass('active');
+		} else {
+			$('.oldActive').next().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+}
+
+// switch to previous
+function prevSlide(){
+	$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':first-child')){
+			$('.slide').last().addClass('active');
+		} else {
+			$('.oldActive').prev().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+}
+});
