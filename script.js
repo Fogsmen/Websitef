c10bef0fbacea9c7bda84c8f1ca722171217bc39
Tarek_1230 <109407872+Fogsmen@users.noreply.github.com>
2023-04-03 06:19:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ document.querySelector('.home').onmouseleave = () =>{
 
 
 
+ 
 
 
 
@@ -61,57 +62,25 @@ document.querySelector('.home').onmouseleave = () =>{
 
 
 
-$(document).ready(function(){
-	// set options
-	var speed = 500;           //fade speed
-	var autoSwitch = true;      //auto slider option
-	var autoSwitchSpeed = 4000; //auto slider speed
-
-	// add initial active class
-	$('.slide').first().addClass('active');
-
-	// hide all slides
-	$('.slide').hide();
-
-	// show first slide
-	$('.active').show();
 
 
-	// next button to go forward
-	$('#next').on('click', nextSlide);
 
+let slideIndex = 0;
+showSlides();
 
-	// previous button to go back
-	$('#prev').on('click', prevSlide);
-
-	// auto slider
-	if(autoSwitch == true){
-		setInterval(nextSlide,autoSwitchSpeed);
-	}
-
-// switches to next slide
-function nextSlide(){
-	$('.active').removeClass('active').addClass('oldActive');
-		if($('.oldActive').is(':last-child')){
-			$('.slide').first().addClass('active');
-		} else {
-			$('.oldActive').next().addClass('active');
-		}
-		$('.oldActive').removeClass('oldActive');
-		$('.slide').fadeOut(speed);
-		$('.active').fadeIn(speed);
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
-// switch to previous
-function prevSlide(){
-	$('.active').removeClass('active').addClass('oldActive');
-		if($('.oldActive').is(':first-child')){
-			$('.slide').last().addClass('active');
-		} else {
-			$('.oldActive').prev().addClass('active');
-		}
-		$('.oldActive').removeClass('oldActive');
-		$('.slide').fadeOut(speed);
-		$('.active').fadeIn(speed);
-}
-});
